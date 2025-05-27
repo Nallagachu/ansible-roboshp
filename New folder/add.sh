0b6d99f70de001c69991aca1d7321f82eb97f58e
durgababu <durgababu.nallagachu@gmail.com>
2025-05-27 10:50:15 +0530

    A

--- New folder/add.sh
+++ New folder/add.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-number = $1
-number2 = $2
+
+number=$1
+number2=$2
+
 echo "The first number is $number"
 echo "The second number is $number2"
-sum = $((number + number2))
-echo "The sum is $sum"
 
+sum=$((number + number2))
+
+echo "The sum is $sum"
