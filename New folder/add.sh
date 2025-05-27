4057b878790d03f257785002fb8f170fe4c0f6fd
durgababu <durgababu.nallagachu@gmail.com>
2025-05-27 10:51:23 +0530

    A

--- New folder/add.sh
+++ New folder/add.sh
@@ -6,6 +6,7 @@ number2=$2
 echo "The first number is $number"
 echo "The second number is $number2"
 
-sum=$((number + number2))
+Subtract=$(($number - $number2))
+echo "The subtraction is $Subtract"
 
 echo "The sum is $sum"
